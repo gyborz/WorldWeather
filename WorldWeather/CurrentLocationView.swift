d1f1ba4c9f9a1c1dc3297c09a99b57d0b9b9193e
borzgy <borzgy95@gmail.com>
2019-07-30 14:42:57 +0200

    Add labels to the current location tab's UI

---
+++ WorldWeather/CurrentLocationView.swift
@@ -0,0 +1,25 @@
+//
+//  CurrentLocationView.swift
+//  WorldWeather
+//
+//  Created by Gyorgy Borz on 2019. 07. 30..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationView: UIView {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var cloudinessLabel: UILabel!
+    @IBOutlet weak var visibilityLabel: UILabel!
+    
+    
+
+}
--- WorldWeather/CurrentLocationViewController.swift
+++ WorldWeather/CurrentLocationViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class CurrentLocationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
