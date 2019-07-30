19c970334e7ebbcbe90bcec062bfc74ae8159202
borzgy <borzgy95@gmail.com>
2019-07-30 10:47:35 +0200

    Initial Commit

---
+++ WorldWeather/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WorldWeather
+//
+//  Created by Gyorgy Borz on 2019. 07. 30..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorldWeather/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WorldWeather
+//
+//  Created by Gyorgy Borz on 2019. 07. 30..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
