9ed63a1d0762a4353b11cabc42195888968e47d8
borzgy <borzgy95@gmail.com>
2019-07-30 15:49:13 +0200

    Add collectionView to UI

--- WorldWeather/CurrentLocationViewController.swift
+++ WorldWeather/CurrentLocationViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        weatherCollectionView.delegate = self
+        weatherCollectionView.dataSource = self
+        weatherCollectionView.backgroundColor = .clear
     }
-
 
 }
 
+extension CurrentLocationViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ForecastViewCell", for: indexPath) as? ForecastCollectionViewCell
+        cell?.hourLabel.text = "15"
+        cell?.degreeLabel.text = "31°"
+        return cell!
+    }
+    
+    
+}
