29ba7ce0205dbc99c6e777124f3fa5eaf6a7ccc7
borzgy <borzgy95@gmail.com>
2019-08-04 10:52:52 +0200

    Set up GetWeatherView and it's outlets

--- WorldWeather/View/GetWeatherView.swift
+++ WorldWeather/View/GetWeatherView.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class GetWeatherView: UIView {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var cloudinessLabel: UILabel!
+    @IBOutlet weak var visibilityLabel: UILabel!
+    @IBOutlet weak var closeButton: UIButton!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        closeButton.backgroundColor = .clear
+        closeButton.layer.cornerRadius = 15
+        closeButton.layer.borderWidth = 1
+        closeButton.layer.borderColor = UIColor.red.cgColor
+    }
+    
+    func updateUI(_ city: String, _ temperature: Int, _ description: String, _ pressure: Int, _ humidity: Int, _ wind: Double, _ cloudiness: Int, _ visibility: Int) {
+        cityLabel.text = city
+        temperatureLabel.text = "\(temperature)°"
+        pressureLabel.text = "Pressure: \(pressure) hPa"
+        humidityLabel.text = "Humidity: \(humidity)%"
+        windLabel.text = "Wind: \(Int(wind * 3.6)) km/h"
+        cloudinessLabel.text = "Cloudiness: \(cloudiness)%"
+        visibilityLabel.text = "Visibility: \(visibility / 1000) km"
+    }
 
 }
