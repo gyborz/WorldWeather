0ab997ee8524b6d02301e3e8bb6a72327fdf5fca
borzgy <borzgy95@gmail.com>
2019-08-04 11:05:55 +0200

    Set up GetWeatherViewController's tableView

--- WorldWeather/Controller/Second tab/GetWeatherViewController.swift
+++ WorldWeather/Controller/Second tab/GetWeatherViewController.swift
@@ -9,11 +9,42 @@
 import UIKit
 
 class GetWeatherViewController: UIViewController {
+    
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
+    @IBOutlet weak var forecastTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        forecastTableView.delegate = self
+        forecastTableView.dataSource = self
+        forecastTableView.register(UINib(nibName: "ForecastTableViewCell", bundle: nil), forCellReuseIdentifier: "ForecastTableViewCell")
+        forecastTableView.rowHeight = 60
+        forecastTableView.allowsSelection = false
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension GetWeatherViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return daysData.count
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastTableViewCell") as! ForecastTableViewCell
+//        cell.dayLabel.text = daysData[indexPath.row].day
+//        cell.hottestLabel.text = "\(daysData[indexPath.row].maxTemperature)°"
+//        cell.coldestLabel.text = "\(daysData[indexPath.row].minTemperature)°"
+        cell.dayLabel.text = "Monday"
+        cell.hottestLabel.text = "25°"
+        cell.coldestLabel.text = "14°"
+        return cell
+    }
+    
 }
