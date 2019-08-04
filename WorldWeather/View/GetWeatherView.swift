60679e7622382477538b7f2b0aceb81df3c2730e
borzgy <borzgy95@gmail.com>
2019-08-04 10:38:34 +0200

    Edit getWeatherView UI

---
+++ WorldWeather/View/GetWeatherView.swift
@@ -0,0 +1,15 @@
+//
+//  GetWeatherView.swift
+//  WorldWeather
+//
+//  Created by Gyorgy Borz on 2019. 08. 04..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class GetWeatherView: UIView {
+
+    
+
+}
