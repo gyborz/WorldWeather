1ad6eb1655ceb75a2e1d3afe043afc9178fa1a29
borzgy <borzgy95@gmail.com>
2019-08-08 14:53:15 +0200

    Set up MapView location services, refactor some code on currentLocationViewC

--- WorldWeather/Controller/Third tab/MapViewController.swift
+++ WorldWeather/Controller/Third tab/MapViewController.swift
@@ -8,15 +8,83 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
     
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
         
+        setupLocationManager()
+    }
+    
+    func setupLocationManager() {
+        /// check if location services are enabled on the device
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            checkLocationAuthorization()
+        } else {
+            // TODO: - alert
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            // TODO: - alert showing how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            // TODO: - alert for restriction problem
+            break
+        case .authorizedAlways:
+            /// won't happen
+            break
+        @unknown default:
+            // TODO: - special alert
+            break
+        }
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
     }
 
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        /// updating the user's location
+        if location.horizontalAccuracy > 0 {
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
 }
